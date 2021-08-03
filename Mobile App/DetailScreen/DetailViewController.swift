d3244acfee1a8fec9578519fc8577640d2a3c2d8
Flavio Haueisen <flavio.haueisen@gmail.com>
2021-08-03 11:03:43 -0300

    Create Home and Detail Storyboards

---
+++ Mobile App/DetailScreen/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  Mobile App
+//
+//  Created by flavio on 03/08/21.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController : UITableViewController {
+    
+}
---
+++ Mobile App/HomeScreen/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  Mobile App
+//
+//  Created by flavio on 03/08/21.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController : UICollectionViewController {
+    
+}
