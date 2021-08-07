a478acccbd855fa6a9e122bc0e49b8a2edda6eec
Flavio Haueisen <flavio.haueisen@gmail.com>
2021-08-07 14:10:06 -0300

    UIImage color extension

---
+++ Mobile App/Extensions/UIImage+Color.swift
@@ -0,0 +1,23 @@
+//
+//  UIImage+Color.swift
+//  Mobile App
+//
+//  Created by flavio on 05/08/21.
+//
+
+import Foundation
+import UIKit
+
+public extension UIImage {
+      convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
+      }
+    }
